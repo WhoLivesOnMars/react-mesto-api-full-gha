a8cf2eec5438e36fc3fed7b662a1c95340d1926c
Daria Khanina <khaninadaria@gmail.com>
2023-10-25 20:08:27 +0300

    fix: changed logic

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,11 +5,6 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 const { SECRET_KEY = 'my-secret-key' } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Требуется авторизация');
-  }
   const token = req.cookies.jwt;
 
   let payload;
