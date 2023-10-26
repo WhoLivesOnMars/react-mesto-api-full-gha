f2d220d63a8b7d5f21705b78957ae5701b2a6fa9
Daria Khanina <khaninadaria@gmail.com>
2023-10-26 23:44:39 +0300

    fix: changed logic

--- backend/app.js
+++ backend/app.js
@@ -27,8 +27,6 @@ app.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
   }
 
